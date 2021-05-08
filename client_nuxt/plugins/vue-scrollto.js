f9c7192ee0a0aece94965c8991814bc620c3d828
dc7290 <dhkh.cba0927@icloud.com>
2021-05-09 06:51:14 +0900

    nuxtフォルダ削除

--- client_nuxt/plugins/vue-scrollto.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import VueScrollTo from 'vue-scrollto';
-
-Vue.use(VueScrollTo);
-
-export default function vueScrollTo(context, inject) {
-  inject('scrollTo', VueScrollTo.scrollTo);
-}
