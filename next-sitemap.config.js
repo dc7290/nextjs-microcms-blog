714ad28d5268540f84ff384bf557891267fd5ad7
dc7290 <dhkh.cba0927@gmail.com>
2022-06-06 11:13:51 +0900

    next-sitemap導入

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
+  generateRobotsTxt: true,
+  autoLastmod: false,
+  priority: false,
+  changefreq: false
+}
+
